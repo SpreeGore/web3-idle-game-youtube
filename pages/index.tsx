5d64ba09d584642adb0a470d9ab997946d0e9157
SpreeGore <89117156+SpreeGore@users.noreply.github.com>
2024-03-12 22:52:42 -0600

    V1

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { NextPage } from "next";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-import ManagerOnDuty from "../components/ManagerOnDuty";
+import Worker from "../components/Worker"
 import Businesses from "../components/Businesses";
 
 const Home: NextPage = () => {
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
         flexDirection: "row",
         width: "100%",
       }}>
-        <ManagerOnDuty />
+        <Worker />
         <Businesses />
       </div>
     </div>
